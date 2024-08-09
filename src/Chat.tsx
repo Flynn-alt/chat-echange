e4aff7c66cd4485bb6ae076238149aa3dfe93581
linzell <linzellart@gmail.com>
2024-08-09 19:57:55 +0200

    add input

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
 // import useChatStore from './store';
+import './Chat.css'
 
 const Chat = () => {
   const [input, setInput] = useState('');
-  // const { messages, addMessage } = useChatStore();
+  const [messages, addMessage] = useState<Array<string>>();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (input.trim()) {
-      // addMessage(input);
+      messages ? addMessage([...messages, input])
+        : addMessage([input]);
       setInput('');
     }
   };
 
   return (
-    // <div>
-    // {messages.map((msg, index) => (
-    //   <div key={index}>{msg}</div>
-    // ))}
-    // </div>
     <div>
+      <div>
+        {messages?.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
       <form onSubmit={handleSubmit}>
         <input
+          className='barre'
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <button type="submit">Send</button>
+        <button
+          className='barre-button'
+          type="submit"
+        >Send</button>
       </form>
     </div>
   );
